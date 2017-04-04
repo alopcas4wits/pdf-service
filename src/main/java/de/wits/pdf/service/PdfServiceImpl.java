7adef3c7d8ea47dc74b63c2ebdcad3e7330e7d90
Jonas Holtorf <Jonas.Holtorf@gmail.com>
2017-04-04 15:27:20 +0200

    another replace

--- src/main/java/de/wits/pdf/service/PdfServiceImpl.java
+++ src/main/java/de/wits/pdf/service/PdfServiceImpl.java
@@ -1,6 +1,7 @@
 package de.wits.pdf.service;
 
 import de.wits.pdf.configuration.FileSystemPathProperties;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.net.URL;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,113 +21,114 @@ import org.springframework.stereotype.Service;
 @Service
 public class PdfServiceImpl implements PdfService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(PdfServiceImpl.class);
-  private static final int RUN_COUNT = 3;
+    private static final Logger LOG = LoggerFactory.getLogger(PdfServiceImpl.class);
+    private static final int RUN_COUNT = 3;
 
-  static final Pattern MEDIA_REGEX = Pattern.compile("img:(https?:\\/\\/[^}]*)");
+    static final Pattern MEDIA_REGEX = Pattern.compile("img:(https?:\\/\\/[^}]*)");
 
-  private transient FileSystemPathProperties fileSystemPathConfig;
+    private transient FileSystemPathProperties fileSystemPathConfig;
 
-  @Autowired
-  public PdfServiceImpl(FileSystemPathProperties fileSystemPathConfig) {
-    this.fileSystemPathConfig = fileSystemPathConfig;
-  }
-
-  @Override
-  public File getPdf(String template) throws PDFCreationFailedException {
-    File file = null;
-    try {
-      File tmpFile = new File(fileSystemPathConfig.getTemporal() + File.separator + UUID.randomUUID(), "template.tex");
-      template = processTemplateMedia(template, tmpFile.getParentFile());
-      FileUtils.writeStringToFile(tmpFile, template, "UTF-8");
-      file = generate(tmpFile);
-    } catch (Exception e) {
-      throw new PDFCreationFailedException("Could not create PDF Document.", e);
+    @Autowired
+    public PdfServiceImpl(FileSystemPathProperties fileSystemPathConfig) {
+        this.fileSystemPathConfig = fileSystemPathConfig;
     }
 
-    return file;
-  }
-
-  public File generate(File templateFile) throws PDFCreationFailedException, IOException {
-    File outputFile = null;
-    try {
-      String pdfLatexPath = fileSystemPathConfig.getPdfLatex();
-      String pdfLatexMode = "--interaction=nonstopmode";
-      String pdfLatexOutput = "--output-directory=" + templateFile.getParent();
-      String shellEscapeCommand = "--shell-escape";
-      String templatePath = templateFile.getAbsolutePath();
-      String[] processArgs = new String[]{
-        pdfLatexPath, templatePath, pdfLatexMode, pdfLatexOutput, shellEscapeCommand
-      };
-      ProcessBuilder processBuilder = new ProcessBuilder(processArgs);
-      processBuilder.redirectErrorStream(true);
-      processBuilder.directory(templateFile.getParentFile());
-
-      for (int i = 1; i <= RUN_COUNT; ++i) {
-        Process process = processBuilder.start();
-        LOG.debug("Doing PDF creating run {} with the arguments: {}", i, processArgs);
-        InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream());
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        StringBuilder outputBuilder = new StringBuilder();
-        String line = null;
-
+    @Override
+    public File getPdf(String template) throws PDFCreationFailedException {
+        File file = null;
         try {
-          while ((line = bufferedReader.readLine()) != null) {
-            outputBuilder.append(line + System.getProperty("line.separator"));
-          }
+            File tmpFile = new File(fileSystemPathConfig.getTemporal() + File.separator + UUID.randomUUID(), "template.tex");
+            template = processTemplateMedia(template, tmpFile.getParentFile());
+            FileUtils.writeStringToFile(tmpFile, template, "UTF-8");
+            file = generate(tmpFile);
+        } catch (Exception e) {
+            throw new PDFCreationFailedException("Could not create PDF Document.", e);
+        }
+
+        return file;
+    }
+
+    public File generate(File templateFile) throws PDFCreationFailedException, IOException {
+        File outputFile = null;
+        try {
+            String pdfLatexPath = fileSystemPathConfig.getPdfLatex();
+            String pdfLatexMode = "--interaction=nonstopmode";
+            String pdfLatexOutput = "--output-directory=" + templateFile.getParent();
+            String shellEscapeCommand = "--shell-escape";
+            String templatePath = templateFile.getAbsolutePath();
+            String[] processArgs = new String[]{
+                pdfLatexPath, templatePath, pdfLatexMode, pdfLatexOutput, shellEscapeCommand
+            };
+            ProcessBuilder processBuilder = new ProcessBuilder(processArgs);
+            processBuilder.redirectErrorStream(true);
+            processBuilder.directory(templateFile.getParentFile());
+
+            for (int i = 1; i <= RUN_COUNT; ++i) {
+                Process process = processBuilder.start();
+                LOG.debug("Doing PDF creating run {} with the arguments: {}", i, processArgs);
+                InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder outputBuilder = new StringBuilder();
+                String line = null;
+
+                try {
+                    while ((line = bufferedReader.readLine()) != null) {
+                        outputBuilder.append(line + System.getProperty("line.separator"));
+                    }
+                } finally {
+                    bufferedReader.close();
+                }
+
+                String outputStream = outputBuilder.toString();
+                try {
+                    int exitCode = process.waitFor();
+
+                    if (exitCode != 0) {
+                        LOG.warn("PDF Creation return non zero exit value: {}", exitCode);
+                        LOG.warn("Output: {}", outputStream);
+                        throw new PDFCreationFailedException("Template wrong. Non Zero exit code: " + exitCode);
+
+                    }
+                } catch (InterruptedException ex) {
+                    LOG.warn("The process pdfLaTeX was interrupted and an exception occurred!", ex);
+                    LOG.warn("Output: {}", outputStream);
+                    throw new PDFCreationFailedException("pdfLaTeX was interrupted", ex);
+                }
+            }
+            outputFile = new File(new FileSystemResource(new File(templateFile.getParent())).getFile(), templateFile.getName().replaceAll(".tex$", ".pdf"));
+            LOG.info("PDF successfully created. Moving to output resource: {}", outputFile.getAbsolutePath());
         } finally {
-          bufferedReader.close();
+            // Clear the temp folder after the work is done
+            //FileUtils.deleteDirectory(templateFile.getParentFile());
         }
 
-        String outputStream = outputBuilder.toString();
-        try {
-          int exitCode = process.waitFor();
+        return outputFile;
+    }
 
-          if (exitCode != 0) {
-            LOG.warn("PDF Creation return non zero exit value: {}", exitCode);
-            LOG.warn("Output: {}", outputStream);
-            throw new PDFCreationFailedException("Template wrong. Non Zero exit code: " + exitCode);
+    String processTemplateMedia(String template, File tempFolder) {
+        Matcher matcher = MEDIA_REGEX.matcher(template);
+        String patchedTemplate = template;
+        int mediaIndex = 0;
+        while (matcher.find()) {
+            String mediaURL = matcher.group(1);
+            mediaURL = mediaURL.replaceAll("}", ""); //FIXME: write a decent regex
+            String filteredURL = mediaURL.replace("\\", "");
+            filteredURL = filteredURL.replace("&amp;", "&");
+            filteredURL = filteredURL.replace("\\&", "&");
+            String mediaName = "img" + mediaIndex + ".png";
+            File mediaFolder = new File(tempFolder, mediaName);
+            LOG.info("Downloading media at " + filteredURL);
+            try {
+                FileUtils.copyURLToFile(new URL(filteredURL), mediaFolder);
+            } catch (IOException ex) {
+                LOG.warn("Ignoring malformed URL: " + mediaURL);
+            }
 
-          }
-        } catch (InterruptedException ex) {
-          LOG.warn("The process pdfLaTeX was interrupted and an exception occurred!", ex);
-          LOG.warn("Output: {}", outputStream);
-          throw new PDFCreationFailedException("pdfLaTeX was interrupted", ex);
+            patchedTemplate = patchedTemplate.replace("img:" + mediaURL, mediaName);
+            mediaIndex++;
         }
-      }
-      outputFile = new File(new FileSystemResource(new File(templateFile.getParent())).getFile(), templateFile.getName().replaceAll(".tex$", ".pdf"));
-      LOG.info("PDF successfully created. Moving to output resource: {}", outputFile.getAbsolutePath());
-    } finally {
-      // Clear the temp folder after the work is done
-      //FileUtils.deleteDirectory(templateFile.getParentFile());
+
+        return patchedTemplate;
     }
-
-    return outputFile;
-  }
-
-  String processTemplateMedia(String template, File tempFolder) {
-    Matcher matcher = MEDIA_REGEX.matcher(template);
-    String patchedTemplate = template;
-    int mediaIndex = 0;
-    while (matcher.find()) {
-      String mediaURL = matcher.group(1);
-      mediaURL = mediaURL.replaceAll("}", ""); //FIXME: write a decent regex
-      String filteredURL = mediaURL.replace("\\", "");
-      filteredURL = filteredURL.replace("\\&", "&");
-      String mediaName = "img" + mediaIndex + ".png";
-      File mediaFolder = new File(tempFolder, mediaName);
-      LOG.info("Downloading media at " + filteredURL);
-      try {
-        FileUtils.copyURLToFile(new URL(filteredURL), mediaFolder);
-      } catch (IOException ex) {
-        LOG.warn("Ignoring malformed URL: " + mediaURL);
-      }
-
-      patchedTemplate = patchedTemplate.replace("img:" + mediaURL, mediaName);
-      mediaIndex++;
-    }
-
-    return patchedTemplate;
-  }
 
 }
